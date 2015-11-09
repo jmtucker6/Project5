a69393b7d9b98307be6a2ddc3e3fd2a163729210
Jacob Tucker <jacob.tucker@oasisdigital.com>
2015-11-08 21:35:13 -0600

    add Heap implementation

---
+++ Project5/Heap.cpp
@@ -0,0 +1,115 @@
+#include "Heap.h"
+
+
+
+Heap::Heap(int s = 10)
+{
+	arr = new Node[s];
+	lastPosition = -1;
+	size = s;
+	numItems = 0;
+}
+
+
+Heap::~Heap()
+{
+	size = 0;
+	delete[] arr;
+}
+
+bool Heap::isHeap(Node n) {
+	return true;
+}
+
+void Heap::insert(Node n) {
+	if (lastPosition == -1) {
+		arr[++lastPosition] = n;
+		numItems++;
+		return;
+	}
+	if (lastPosition >= size - 1) {
+		expandArr();
+	}
+	numItems++;
+	arr[++lastPosition] = n;
+	bubbleUp(lastPosition);
+	return;
+}
+
+void Heap::bubbleUp(int pos) {
+	if (pos < 1)
+		return;
+	if (arr[pos].priority < arr[getParentIndex(pos)].priority) {
+		Node temp;
+		temp = arr[getParentIndex(pos)];
+		arr[getParentIndex(pos)] = arr[pos];
+		arr[pos] = temp;
+		pos = getParentIndex(pos);
+		bubbleUp(pos);
+	}
+	return;
+}
+
+void Heap::bubbleDown(int pos) {
+	int min = pos;
+	if (getLeftIndex(pos) <= lastPosition && arr[getLeftIndex(pos)].priority < arr[min].priority) {
+		min = getLeftIndex(pos);
+	}
+	if (getRightIndex(pos) <= lastPosition && arr[getRightIndex(pos)].priority < arr[min].priority) {
+		min = getRightIndex(pos);
+	}
+	if (min != pos) {
+		Node temp = arr[pos];
+		arr[pos] = arr[min];
+		arr[min] = temp;
+		bubbleDown(min);
+	}
+}
+
+Node Heap::removeMin() {
+	Node temp = arr[lastPosition];
+	arr[lastPosition] = arr[0];
+	arr[0] = temp;
+	temp = arr[lastPosition];
+	arr[lastPosition].content = "";
+	arr[lastPosition].priority = 0;
+	lastPosition--;
+	bubbleDown(0);
+	numItems--;
+	return temp;
+}
+
+int Heap::getParentIndex(int pos) {
+	return (pos - 1) / 2;
+}
+
+int Heap::getLeftIndex(int pos) {
+	return pos * 2 + 1;
+}
+
+int Heap::getRightIndex(int pos) {
+	return pos * 2 + 2;
+}
+
+int Heap::count() {
+	return numItems;
+}
+
+void Heap::expandArr() {
+	Node *temp;
+	temp = new Node[size * 2];
+	for (int i = 0; i < size; i++) {
+		temp[i] = arr[i];
+	}
+	delete[] arr;
+	arr = temp;
+	size *= 2;
+	delete[] temp;
+}
+
+void Heap::displayArr() {
+	for (int i = 0; i < size; i++) {
+		std::cout << arr[i].content << " ";
+	}
+	std::cout << std::endl;
+}
